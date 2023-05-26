4c0b7018db1c09c5b4c3820c9ee43d7bb7a0087e
Jake012345 <nagyerik012345@gmail.com>
2023-05-26 23:54:21 +0200

    -edit screen is developing -global warning window with accept functionality is created -subject list is done, deletion is required to be writen
    Signed-off-by: Jake012345 <nagyerik012345@gmail.com>

--- scenes_scripts/EditSubjects.gd
+++ scenes_scripts/EditSubjects.gd
@@ -1,20 +1,23 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-   pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#   pass
+onready var list_subjects = get_node("SubjectSelector")
+onready var list_topics = get_node("TopicSelector")
 
 
 func refresh_data():
+   list_subjects.clear()
+   for i in GlobalDatabase.subjects:
+      list_subjects.add_item(i.text)
+      pass
+   pass
+
+
+func delete_subject():
+   GlobalFunctions.show_warning(self, "subject_deletion_accepted", \
+   "Do you really want to delete the selected Subject(s)?" + "\n" + \
+   "(The connected Topics and Terms are going to be deleted as well.)")
+   pass
+
+func subject_deletion_accepted():
+   #write the code of subject deletion
    pass
