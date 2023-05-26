0375f853a814ab5b6d4f7605a69581ee110074b3
Jake012345 <nagyerik012345@gmail.com>
2023-05-26 23:54:21 +0200

    -edit screen is developing -global warning window with accept functionality is created -subject list is done, deletion is required to be writen
    Signed-off-by: Jake012345 <nagyerik012345@gmail.com>

--- scenes_scripts/EditScreenContainer.gd
+++ scenes_scripts/EditScreenContainer.gd
@@ -2,23 +2,17 @@ extends TabContainer
 
 onready var selector_button_terms = get_parent().get_node("HBoxContainer/ButtonEditTerms")
 onready var selector_button_subjects = get_parent().get_node("HBoxContainer/ButtonEditSubjects")
-onready var selector_button_topics = get_parent().get_node("HBoxContainer/ButtonEditTopic")
-signal refresh_data
 
 func _ready():
    current_tab = 0
+   selector_button_terms.pressed = true
    pass
 
 func change_edit_mode(to: int):
-   current_tab = to
-   emit_signal("refresh_data")
-   if to == 0:
-      selector_button_subjects.pressed = false
-      selector_button_topics.pressed = false
-   elif to == 1:
-      selector_button_terms.pressed = false
-      selector_button_topics.pressed = false
-   else:
-      selector_button_subjects.pressed = false
-      selector_button_terms.pressed = false
-   pass
+   if to != current_tab:
+      current_tab = to
+      get_child(current_tab).refresh_data()
+      if to == 0:
+         selector_button_subjects.pressed = false
+      elif to == 1:
+         selector_button_terms.pressed = false
--- scenes_scripts/global_functions.gd
+++ scenes_scripts/global_functions.gd
@@ -2,6 +2,8 @@ extends Node
 
 
 var font = load("res://lib/font/CourierPrime-Regular.ttf")
+var warning_accepted = false
+signal accept_warning
 
 func set_font(node: Control, font_size: int = 32):
    var tmp_theme = Theme.new()
@@ -9,4 +11,51 @@ func set_font(node: Control, font_size: int = 32):
    tmp_theme.default_font.font_data = font
    tmp_theme.default_font.size = font_size
    node.theme = tmp_theme
+   pass
+
+func show_warning(parent: Node, triggered_func: String, text: String):
+   var warning = PopupDialog.new()
+   var label = Label.new()
+   var ok = Button.new()
+   var cancel = Button.new()
+   var clicked = false
+   parent.add_child(warning)
+   warning.add_child(label)
+   warning.rect_size.x = 780
+   warning.rect_size.y = 440
+   warning.popup_exclusive = true
+   set_font(label)
+   label.rect_size.x = warning.rect_size.x -20
+   label.rect_size.y = warning.rect_size.y -110
+   label.rect_position.x = 10
+   label.rect_position.y = 10
+   label.autowrap = true
+   label.clip_text = true
+   label.text = text
+   warning.add_child(ok)
+   warning.add_child(cancel)
+   ok.rect_position.x = 430
+   ok.rect_position.y = 270
+   ok.rect_size.x = 200
+   ok.rect_size.y = 100
+   ok.text = "Ok"
+   set_font(ok)
+   cancel.rect_position.x = 130
+   cancel.rect_position.y = 270
+   cancel.rect_size.x = 200
+   cancel.rect_size.y = 100
+   cancel.text = "Cancel"
+   set_font(cancel)
+   ok.connect("pressed", self, "warning_answered", [true])
+   cancel.connect("pressed", self, "warning_answered", [false])
+   warning.popup_centered()
+   yield(self, "accept_warning")
+   if warning_accepted:
+      parent.call(triggered_func)
+   warning.call_deferred("free")
+   pass
+
+func warning_answered(accepted: bool):
+   warning_accepted = accepted
+   emit_signal("accept_warning")
    pass
